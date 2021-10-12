1c87acfb7c29b352f3d2905b89cb1fec742b7806
Carlos Pijanowski <carlos.pijanowski@gmail.com>
2021-10-12 12:23:41 -0300

    Initial commit

---
+++ src/main/java/methodReference/ExemplosComMethodReference.java
@@ -0,0 +1,14 @@
+package methodReference;
+
+public class ExemplosComMethodReference {
+
+
+    // existem alguns tipos de methods reference
+    // ao menos 4 os mais importantes
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
