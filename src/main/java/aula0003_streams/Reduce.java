331b69047643338a9535ba2f00c0590e1e862a90
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-12 21:36:26 -0300

    atualizacaoes Inteiros.java

--- src/main/java/aula0003_streams/Reduce.java
+++ src/main/java/aula0003_streams/Reduce.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 public class Reduce {
 
     //reduce -> pega todos os elementos em um unico elemento
+    //todo existe mais coisas a estudar sobre esse topico em https://www.youtube.com/watch?v=2aHElyENpuM&t=887s
 
     public static void main(String[] args) {
 
         List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         reduceSoma(lista);
+        reduceSubtracao(lista);
         reduceMultiplicacao(lista);
         reduceComStrings();
 
@@ -23,6 +25,12 @@ public class Reduce {
                 .reduce((n1, n2) -> n1 + n2);
 
         System.out.println(acumuladorDeResultado.get());
+    }
+
+    public static void reduceSubtracao(List<Integer> lista){
+        System.err.println("Não faça a subtracao pois não é associativa.\n" +
+                "A api do java nao garante que isso sempre mostrará um resultado de fato esperado.\n" +
+                "Exemplo.: (1-2) - (3-4) - (5-6) não mostra um resultado diferente de paralel");
     }
 
     public static void reduceMultiplicacao(List<Integer> lista){
