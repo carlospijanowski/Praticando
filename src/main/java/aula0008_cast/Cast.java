209b2b36a56798119b4266d135e031ad4e38e313
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-16 19:46:59 -0300

    mudancas

--- src/main/java/aula0008_cast/Cast.java
+++ src/main/java/aula0008_cast/Cast.java
@@ -1,15 +1,23 @@
 package aula0008_cast;
 
 import aula0008_cast.model.Conta;
+import aula0008_cast.model.ContaCorrente;
 import aula0008_cast.model.ContaPoupanca;
 
 public class Cast {
 
     public static void main(String[] args) {
 
-        Conta contaPoupanca = new ContaPoupanca();
+        ContaPoupanca contaPoupanca = new ContaPoupanca();
         contaPoupanca.setConta("0001");
-        System.out.println(contaPoupanca.getConta());
+
+        ContaCorrente contaCorrente = new ContaCorrente();
+
+        Conta conta = new Conta();
+
+        conta = contaPoupanca;
+
+
 
     }
 }
--- src/main/java/aula0008_cast/model/Conta.java
+++ src/main/java/aula0008_cast/model/Conta.java
@@ -3,7 +3,7 @@ package aula0008_cast.model;
 import lombok.Getter;
 
 
-@Getter
+@Data
 public class Conta {
 
     int agencia;
