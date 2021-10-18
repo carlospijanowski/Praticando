b45172c079cc90e78c9981a7e6b7daebe4247a23
Carlos <carlospc1978@gmail.com>
2021-10-17 21:30:47 -0300

    ajustes

--- src/main/java/aula_0009_classes_Estaticas/ClasseEstatica.java
+++ src/main/java/aula_0009_classes_Estaticas/ClasseEstatica.java
@@ -18,16 +18,32 @@ public class ClasseEstatica {
 
         static class EssaEhEstatica{
             void imprimir(){
-                System.out.println("imprimir estacica");
+                System.out.println("imprimir "+tipoDaClasse.ESTATICA);
             }
         }
 
         class EssaNaoEhEstatica{
             void imprimir(){
-                System.out.println("imprimir nao estatica");
+                System.out.println("imprimir "+tipoDaClasse.NAOESTATICA);
             }
         }
 
+    }
+
+    public enum tipoDaClasse{
+        ESTATICA("estatica"),
+        NAOESTATICA("não estática");
+
+        String tipo;
+
+        tipoDaClasse(String tipo) {
+            this.tipo = tipo;
+        }
+
+        @Override
+        public String toString() {
+            return tipo;
+        }
     }
 
 
