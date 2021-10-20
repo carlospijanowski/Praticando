4fb1bda6eb8c39dba55bb711ed74ee443474fa11
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-10-20 13:35:13 -0300

    mudancas

---
+++ src/main/java/aula0000_naoRastreadosAinda/StringsEmJava.java
@@ -0,0 +1,19 @@
+package aula0000_naoRastreadosAinda;
+
+public class StringsEmJava {
+
+    static String nome="Carlos";
+
+    public static void main(String[] args) {
+
+        if(nome.equalsIgnoreCase("Carlos")){
+            System.out.println("no if");
+        }else{
+            System.out.println("no else");
+        }
+
+    }
+
+
+
+}
