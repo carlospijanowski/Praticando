0c04e16e400faf5ab90976142c22776fd0b091a2
Carlos <carlospc1978@gmail.com>
2021-10-19 22:05:35 -0300

    atualizacoes

--- src/main/java/model/Humano.java
+++ src/main/java/model/Humano.java
@@ -1,21 +1,5 @@
 package model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Humano {
-
-    String raca;
-
-    public class Homem extends Humano {
-
-        public void main(String[] args) {
-            Homem homem = new Homem();
-            homem.raca = "Bravo";
-        }
-
-    }
 
 }
