92ee38ae9ab84278e674d0253d8ae5cccda3aaeb
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-07 20:30:33 -0300

    teste com ENUM - AULA 80

---
+++ src/main/java/aula_0009_Enum/Teste.java
@@ -0,0 +1,12 @@
+package aula_0009_Enum;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Carlos",TipoDePessoa.PESSOAFISICA);
+
+        System.out.println(cliente);
+
+    }
+
+}
