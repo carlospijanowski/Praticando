7a2b12a718d478fa7eb8b1be3868889ef6e77f1f
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-09 18:38:22 -0300

    ajustes nos pacotes

--- src/main/java/aula0008_cast/Cast.java
+++ src/main/java/aula0008_cast/Cast.java
@@ -1,6 +1,7 @@
 package aula0008_cast;
 
 import aula0008_cast.model.Conta;
+import aula0008_cast.model.ContaCorrente;
 import aula0008_cast.model.ContaPoupanca;
 
 public class Cast {
@@ -8,6 +9,9 @@ public class Cast {
     public static void main(String[] args) {
 
         ContaPoupanca contaPoupanca = new ContaPoupanca();
+        ContaCorrente contaCorrente = new ContaCorrente();
+        Conta conta = (Conta) contaPoupanca;
+
 
     }
 }
