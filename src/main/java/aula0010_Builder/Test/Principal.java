95eacdeff586cc7ed72918f433ab73ba6e7bf613
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-09 19:05:29 -0300

    aula010_Buider

---
+++ src/main/java/aula0010_Builder/Test/Principal.java
@@ -0,0 +1,18 @@
+package aula0010_Builder.Test;
+
+import aula010_Builder.dominio.Person;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Person pessoa = new Person.PersonBuilder()
+                .nome("Carlos")
+                .email("carlospc1978@gmail.com")
+                .build();
+
+        System.out.println(pessoa.getNome());
+
+    }
+
+}
