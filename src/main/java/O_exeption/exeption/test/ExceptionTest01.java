539762ee56c171ef8cc27fca491fbf4ef26b7361
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-21 10:29:40 -0300

    atualizacoes

---
+++ src/main/java/O_exeption/exeption/test/ExceptionTest01.java
@@ -0,0 +1,14 @@
+package O_exeption.exeption.test;
+
+public class ExceptionTest01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("caracter de scape \\ por isso imprime apenas uma ");
+
+
+    }
+
+//    pri
+
+}
