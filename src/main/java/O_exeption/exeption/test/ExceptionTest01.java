2f8d96e571bc6a478b1ddc75547c0e0c76d8e9bc
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-11-21 10:47:05 -0300

    estudo de exceções

--- src/main/java/O_exeption/exeption/test/ExceptionTest01.java
+++ src/main/java/O_exeption/exeption/test/ExceptionTest01.java
@@ -1,14 +1,28 @@
 package O_exeption.exeption.test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ExceptionTest01 {
 
     public static void main(String[] args) {
 
         System.out.println("caracter de scape \\ por isso imprime apenas uma ");
 
+        criarArquivo();
 
     }
 
-//    pri
+    private static void criarArquivo(){
+        File file = new File("arquivos\\teste.txt");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println("try -> "+isCreated);
+        } catch (IOException e) {
+            e.getMessage();
+            System.out.println("mensagem do erro -> "+e);
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/main/java/O_exeption/runtime/test/RuntimeExceptionTest.java
@@ -0,0 +1,4 @@
+package O_exeption.runtime.test;
+
+public class RuntimeExceptionTest {
+}
